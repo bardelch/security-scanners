8ce493f5358500705fef94d26bb8f2262ffb3ec5
Chuck Bardel <chuck.bardel@gmail.com>
2020-12-09 18:48:52 +0000

    more results for openssl

--- results-cmd.sh
+++ results-cmd.sh
@@ -6,3 +6,6 @@ echo test/stack-mod.c
 graudit/graudit -d graudit/signatures/c.db test/stack-mod.c 
 flawfinder-2.0.11/flawfinder test/stack-mod.c 
 splint test/stack-mod.c 
+graudit/graudit -d graudit/signatures/c.db openssl-0.9.8za/ssl/d1_both.c 
+flawfinder-2.0.11/flawfinder openssl-0.9.8za/ssl/d1_both.c 
+splint -I openssl-0.9.8za/ -I openssl-0.9.8za/include/ openssl-0.9.8za/ssl/d1_both.c 
