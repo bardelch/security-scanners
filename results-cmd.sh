ade4283599ae0122b3c3d69ab167b842eca30854
Chuck Bardel <chuck.bardel@gmail.com>
2020-12-09 20:18:37 +0000

    updated results to split outputs

--- results-cmd.sh
+++ results-cmd.sh
@@ -1,11 +1,21 @@
 echo test/stack.c
+echo graudit/graudit 
 graudit/graudit -d graudit/signatures/c.db test/stack.c 
+echo flawfinder
 flawfinder-2.0.11/flawfinder test/stack.c 
+echo splint
 splint test/stack.c 
 echo test/stack-mod.c
+echo graudit/graudit 
 graudit/graudit -d graudit/signatures/c.db test/stack-mod.c 
+echo flawfinder
 flawfinder-2.0.11/flawfinder test/stack-mod.c 
+echo splint
 splint test/stack-mod.c 
+echo openssl-0.9.8za/ssl/d1_both.c 
+echo graudit/graudit 
 graudit/graudit -d graudit/signatures/c.db openssl-0.9.8za/ssl/d1_both.c 
+echo flawfinder
 flawfinder-2.0.11/flawfinder openssl-0.9.8za/ssl/d1_both.c 
+echo splint
 splint -I openssl-0.9.8za/ -I openssl-0.9.8za/include/ openssl-0.9.8za/ssl/d1_both.c 
---
+++ setup.sh
@@ -0,0 +1,5 @@
+apt-get update
+apt-get upgrade
+apt-get install splint
+apt-get install build-essential
+apt-get install python
