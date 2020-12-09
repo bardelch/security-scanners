7ce3d1778ef4e75c4a48ae0c3f33196d554ed327
Chuck Bardel <chuck.bardel@gmail.com>
2020-12-09 03:42:21 +0000

    added splint results

--- results-cmd.sh
+++ results-cmd.sh
@@ -1,6 +1,8 @@
 echo test/stack.c
 graudit/graudit -d graudit/signatures/c.db test/stack.c 
 flawfinder-2.0.11/flawfinder test/stack.c 
+splint test/stack.c 
 echo test/stack-mod.c
 graudit/graudit -d graudit/signatures/c.db test/stack-mod.c 
 flawfinder-2.0.11/flawfinder test/stack-mod.c 
+splint test/stack-mod.c 
